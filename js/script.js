db41d5195de571284434ca4e8f14fa8074cd26b1
Marie-Louise Annan <m.annan@wellcome.ac.uk>
2018-10-31 11:23:03 +0000

    add more methods

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-function Recipe(id, name) {
+function Recipe(id, label, description, portion, preparation, cook, level, spice, method) {
     this.id = id;
-    this.name = name;
+    this.label = label;
+    this.description = description;
+    this.portion = portion;
+    this.preparation = preparation;
+    this.cook = cook;
+    this.level = level;
+    this.spice = spice;
+    this.method = method;
+
+    //getter
     this.getId = function() {
         console.log(this.id);
     }
-    this.getName = function() {
-        console.log(this.name);
+    this.getLabel() = function() {
+        console.log(this.label);
     }
 
 };
